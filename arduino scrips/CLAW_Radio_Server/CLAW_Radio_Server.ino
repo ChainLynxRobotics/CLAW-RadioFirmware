9bbee9ba5eb4ad918c6c8fac15b910c23f2acc34
Peter Wojt <156293185+peterwojt@users.noreply.github.com>
2024-10-24 17:22:23 -0700

    Added LoRa transmit and receive functionalities

--- arduino scrips/CLAW_Radio_Server/CLAW_Radio_Server.ino
+++ arduino scrips/CLAW_Radio_Server/CLAW_Radio_Server.ino
@@ -24,12 +24,34 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+//LoRa Setup
+
+#include <LoRa.h>
+#include "LoRaBoards.h"
+
+#ifndef CONFIG_RADIO_FREQ
+#define CONFIG_RADIO_FREQ           868.0
+#endif
+#ifndef CONFIG_RADIO_OUTPUT_POWER
+#define CONFIG_RADIO_OUTPUT_POWER   17
+#endif
+#ifndef CONFIG_RADIO_BW
+#define CONFIG_RADIO_BW             125.0
+#endif
+
+#if !defined(USING_SX1276) && !defined(USING_SX1278)
+#error "LoRa example is only allowed to run SX1276/78. For other RF models, please run examples/RadioLibExamples
+#endif
+
+
+
+
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristic = NULL;
 uint32_t value = 0;
 String serverName;
 bool location;
-
+bool transmit_mode = false;
 
 // TODO
 // set up password stuff
@@ -121,8 +143,86 @@ void setup() {
   pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
   Serial.println("Waiting a client connection to notify...");
+
+  setup_LoRa();
+
 }
 
 void loop() {
+    // Example LoRa receiver code
 
+    // try to parse packet
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) {
+        // received a packet
+        Serial.print("Received packet '");
+
+        String recv = "";
+        // read packet
+        while (LoRa.available()) {
+            recv += (char)LoRa.read();
+        }
+
+        Serial.println(recv);
+
+        // print RSSI of packet
+        Serial.print("' with RSSI ");
+        Serial.println(LoRa.packetRssi());
+        if (u8g2) {
+            u8g2->clearBuffer();
+            char buf[256];
+            u8g2->drawStr(0, 12, "Received OK!");
+            u8g2->drawStr(0, 26, recv.c_str());
+            snprintf(buf, sizeof(buf), "RSSI:%i", LoRa.packetRssi());
+            u8g2->drawStr(0, 40, buf);
+            snprintf(buf, sizeof(buf), "SNR:%.1f", LoRa.packetSnr());
+            u8g2->drawStr(0, 56, buf);
+            u8g2->sendBuffer();
+        }
+    }
+}
+
+void setup_LoRa() {
+    setupBoards();
+    // When the power is turned on, a delay is required.
+    delay(1500);
+
+    Serial.println("LoRa Receiver");
+
+#ifdef  RADIO_TCXO_ENABLE
+    pinMode(RADIO_TCXO_ENABLE, OUTPUT);
+    digitalWrite(RADIO_TCXO_ENABLE, HIGH);
+#endif
+
+    LoRa.setPins(RADIO_CS_PIN, RADIO_RST_PIN, RADIO_DIO0_PIN);
+    if (!LoRa.begin(CONFIG_RADIO_FREQ * 1000000)) {
+        Serial.println("Starting LoRa failed!");
+        while (1);
+    }
+
+    LoRa.setTxPower(CONFIG_RADIO_OUTPUT_POWER);
+
+    LoRa.setSignalBandwidth(CONFIG_RADIO_BW * 1000);
+
+    LoRa.setSpreadingFactor(10);
+
+    LoRa.setPreambleLength(16);
+
+    LoRa.setSyncWord(0xAB);
+
+    LoRa.disableCrc();
+
+    LoRa.disableInvertIQ();
+
+    LoRa.setCodingRate4(7);
+    
+    // put the radio into receive mode
+    LoRa.receive();
+
+}
+
+void transmit_data_via_LoRa(String data) {
+    LoRa.beginPacket();
+    LoRa.print(data);
+    LoRa.endPacket();
 }
